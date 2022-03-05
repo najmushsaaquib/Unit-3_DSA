0b93220aa14acfb13673567ce3b57c09721ba6ec
Najmush Saaquib <saaquibnajmush@gmail.com>
2022-03-05 15:17:11 +0530

    initial folder

---
+++ 5th March (Day 4)/rectree.js
@@ -0,0 +1,9 @@
+// Fibbonacci Series
+
+function fib(n){
+    if(n==0 || n==1) return n
+    return fib(n-1) + fib(n-2) 
+}
+
+fib(4)
+console.log(fib(4))
---
+++ 5th March (Day 4)/recursion.js
@@ -0,0 +1,15 @@
+let a = 2;
+
+function sum (a) {
+    if(a%2==0) return 
+
+    a=a+1
+    a=a*3
+    console.log(a)
+    return
+}
+
+
+// console.log(sum(a));
+sum(a)
+console.log(a)
