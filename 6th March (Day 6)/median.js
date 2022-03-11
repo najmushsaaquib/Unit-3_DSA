d44270dec0594f8b8fe42dce8a620522dcd2f39f
Najmush Saaquib <saaquibnajmush@gmail.com>
2022-03-12 00:05:50 +0530

    updated

--- 6th March (Day 6)/median.js
+++ 6th March (Day 6)/median.js
@@ -1,19 +1,17 @@
-let nums1 = [1,2];
-let nums2 = [4,3]
+let nums1 = [1, 2];
+let nums2 = [4, 3];
 
-let arr = nums1.concat(nums2)
-arr.sort((a,b) => {return (a-b)})
-let n = arr.length
+let arr = nums1.concat(nums2);
+arr.sort((a, b) => {
+  return a - b;
+});
+let n = arr.length;
 
-if(n%2 != 0) {
-    console.log(arr[Math.floor(n/2)])
-}else{
-
-    let sum = arr[n/2]+ arr[(n/2)-1]
-    console.log(sum/2)
-
+if (n % 2 != 0) {
+  console.log(arr[Math.floor(n / 2)]);
+} else {
+  let sum = arr[n / 2] + arr[n / 2 - 1];
+  console.log(sum / 2);
 }
-
-
 
 // console.log(arr)
