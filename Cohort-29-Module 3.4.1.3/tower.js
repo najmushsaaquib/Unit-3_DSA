b34eedf00a4755fcdc3b035433af5909ad8bf4f2
Najmush Saaquib <saaquibnajmush@gmail.com>
2022-03-14 09:23:18 +0530

    'changes'

--- Cohort-29-Module 3.4.1.3/tower.js
+++ Cohort-29-Module 3.4.1.3/tower.js
@@ -1,37 +1,7 @@
-function runProgram(input = "") {
-  input = input.trim().split("\n");
-  let disc = +input[0];
-  let A = +input[1];
-  let B = +input[2];
-  let C = +input[3];
-  toh(disc, A, B, C);
+let n = 148;
+let res = ""
+for(let i =0; i<3; i++){
+  res+=n
 }
 
-function toh(disc, A, B, C) {
-  if (disc == 0) return;
-
-  toh(disc - 1, A, C, B);
-  console.log(`${disc}[${A} -> ${B}`);
-  toh(disc - 1, C, B, A);
-}
-
-if (process.env.USER === "") {
-  runProgram(``);
-} else {
-  process.stdin.resume();
-  process.stdin.setEncoding("ascii");
-  let read = "";
-  process.stdin.on("data", function (input) {
-    read += input;
-  });
-  process.stdin.on("end", function () {
-    read = read.replace(/\n$/, "");
-    read = read.replace(/\n$/, "");
-    runProgram(read);
-  });
-  process.on("SIGINT", function () {
-    read = read.replace(/\n$/, "");
-    runProgram(read);
-    process.exit(0);
-  });
-}
+console.log(res)
