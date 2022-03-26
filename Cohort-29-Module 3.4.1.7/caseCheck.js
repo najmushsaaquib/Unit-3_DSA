2d06463994db8ff278bd52317d4928cbfc04e9be
najmushsaaquib <saaquibnajmush@gmail.com>
2022-03-27 00:54:36 +0530

    maze

---
+++ Cohort-29-Module 3.4.1.7/caseCheck.js
@@ -0,0 +1,7 @@
+let z = "K"
+
+if(z >= "a" && z <= "z"){
+    console.log("Lowercase");
+} else {
+    console.log("Uppercase");
+}
---
+++ Cohort-29-Module 3.4.1.7/maze.js
@@ -0,0 +1,38 @@
+function runProgram(input = "") {
+  input = input.trim().split("\n");
+  let tc = +input[0];
+  let line = 1;
+  for (let i = 0; i < tc; i++) {
+    let n = +input[line++];
+    let arr = input[line++].trim().split(" ").map(Number);
+    // console.log(recursionSum(arr, 0));
+  }
+}
+
+function recursionSum(arr, ind) {
+  if (ind == arr.length) return 0;
+  let sum = recursionSum(arr, ind + 1);
+  let out = arr[ind] + sum;
+  return out;
+}
+
+if (process.env.USER === "") {
+  runProgram(``);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
